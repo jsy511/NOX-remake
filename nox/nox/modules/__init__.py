85dc41e7cead024a0cf9bf06a01a2e4d00ca89f9
jsy511 <jaydenfreeman9876@gmail.com>
2026-09-20 09:01:21 +0100

    Create nox/modules/__init__.py

---
+++ nox/nox/modules/__init__.py
@@ -0,0 +1,14 @@
+"""
+NOX module system.
+
+Each cybersecurity feature can eventually be implemented
+as its own module.
+"""
+
+MODULES = [
+    "network",
+    "crypto",
+    "web",
+    "osint",
+    "forensics",
+]
