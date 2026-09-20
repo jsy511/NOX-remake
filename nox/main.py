1555bd939b11b9651f04ef4e435d41b1f901781b
jsy511 <jaydenfreeman9876@gmail.com>
2026-09-20 08:57:58 +0100

    Create nox/main.py

---
+++ nox/main.py
@@ -0,0 +1,12 @@
+from nox import __version__
+
+
+def main():
+    print("NOX")
+    print(f"Version {__version__}")
+    print()
+    print("NOX is starting...")
+
+
+if __name__ == "__main__":
+    main()
