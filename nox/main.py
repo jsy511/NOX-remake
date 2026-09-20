8fd3ff89760e75da216a083be3a3bb28253caeea
jsy511 <jaydenfreeman9876@gmail.com>
2026-09-20 08:59:23 +0100

    Update main.py

--- nox/main.py
+++ nox/main.py
@@ -1,10 +1,10 @@
 from nox import __version__
+from nox.banner import BANNER
 
 
 def main():
-    print("NOX")
-    print(f"Version {__version__}")
-    print()
+    print(BANNER)
+    print(f"NOX v{__version__}")
     print("NOX is starting...")
 
 
